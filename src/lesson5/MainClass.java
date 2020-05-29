30ed2537ba3100398d54afef5d00c2614c834d4c
Stas Zareckii <zareckii@gmail.com>
2020-05-29 16:04:58 +0300

    Homework5

---
+++ src/lesson5/MainClass.java
@@ -0,0 +1,33 @@
+package lesson5;
+
+public class MainClass {
+    static class MyRunnableClass implements Runnable {
+        private String nameThread;
+
+        public MyRunnableClass(String nameThread) {
+            this.nameThread = nameThread;
+        }
+
+        public String getNameThread() {
+            return nameThread;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    Thread.sleep(100);
+                    System.out.println( getNameThread() + ": " + i);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new Thread(new MyRunnableClass("Thread 1")).start();
+        new Thread(new MyRunnableClass("Thread 2")).start();
+    }
+}
---
+++ src/lesson5/dz/MyArrayThead.java
@@ -0,0 +1,11 @@
+package lesson5.dz;
+
+public class MyArrayThead implements Runnable{
+
+
+
+    @Override
+    public void run() {
+
+    }
+}
